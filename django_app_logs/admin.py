03b0b97da461af99b6df3b90a255c6a64140f79c
Cisse Anzoumana <kestyraf@gmail.com>
2025-12-26 07:40:52 +0000

    update admin

--- django_app_logs/admin.py
+++ django_app_logs/admin.py
@@ -143,7 +143,7 @@ class ActionLogAdmin(admin.ModelAdmin):
     def object_history_link(self, obj):
         """Lien vers l'historique complet de l'objet."""
         url = reverse(
-            'admin:history_object_history',
+            f'{self.admin_site.name}:history_object_history',
             args=[obj.content_type_id, obj.object_id]
         )
         count = ActionLog.objects.filter(
